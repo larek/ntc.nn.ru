175da499921428cace5851e4ae4406ca6e85d590
Alexandr Tikhonov <t9101029991@gmail.com>
2016-04-12 19:16:25 +0300

    correct tours template

--- basic/views/site/tours.php
+++ basic/views/site/tours.php
@@ -14,12 +14,12 @@ $this->title = $title;
 
 <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
     <div class="tours__item" style="background-image: url('http://www.abc34.ru/preview/original/pic/517_1.jpg')">
-        <a href="/ekskursii/avtobusnye/v-poiskah-deda-moroza/" class="tours__item-link"></a>
+        <a href="#" class="tours__item-link"></a>
 
                 
         <div class="tours__item-description">
             <div class="tours__item-title-wrapper">
-                <a href="/ekskursii/avtobusnye/v-poiskah-deda-moroza/" class="tours__item-title">Поездка в Дивеево</a>
+                <a href="#" class="tours__item-title">Поездка в Дивеево</a>
             </div>
 
             <div class="tours__item-params">
@@ -36,12 +36,12 @@ $this->title = $title;
 
 <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
     <div class="tours__item" style="background-image: url('http://www.sto-dorog.ru/media/_File/nizhobl/nnov/boldino15.jpg')">
-        <a href="/ekskursii/avtobusnye/v-poiskah-deda-moroza/" class="tours__item-link"></a>
+        <a href="#" class="tours__item-link"></a>
 
                 
         <div class="tours__item-description">
             <div class="tours__item-title-wrapper">
-                <a href="/ekskursii/avtobusnye/v-poiskah-deda-moroza/" class="tours__item-title">Поездка в Болдино</a>
+                <a href="#" class="tours__item-title">Поездка в Болдино</a>
             </div>
 
             <div class="tours__item-params">
@@ -58,12 +58,12 @@ $this->title = $title;
 
 <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
     <div class="tours__item" style="background-image: url('http://atnspb.ru/upload/image/cd6ae8abb99a6ec6de1dc502fde26a5326b1e36f.jpeg')">
-        <a href="/ekskursii/avtobusnye/v-poiskah-deda-moroza/" class="tours__item-link"></a>
+        <a href="#" class="tours__item-link"></a>
 
                 
         <div class="tours__item-description">
             <div class="tours__item-title-wrapper">
-                <a href="/ekskursii/avtobusnye/v-poiskah-deda-moroza/" class="tours__item-title">Усадьба Руковишниковых</a>
+                <a href="#" class="tours__item-title">Усадьба Руковишниковых</a>
             </div>
 
             <div class="tours__item-params">
@@ -80,12 +80,12 @@ $this->title = $title;
 
 <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
     <div class="tours__item" style="background-image: url('http://www.niceimage.ru/large/201305/27363.jpg')">
-        <a href="/ekskursii/avtobusnye/v-poiskah-deda-moroza/" class="tours__item-link"></a>
+        <a href="#" class="tours__item-link"></a>
 
                 
         <div class="tours__item-description">
             <div class="tours__item-title-wrapper">
-                <a href="/ekskursii/avtobusnye/v-poiskah-deda-moroza/" class="tours__item-title">Столица хохломы</a>
+                <a href="#" class="tours__item-title">Столица хохломы</a>
             </div>
 
             <div class="tours__item-params">
